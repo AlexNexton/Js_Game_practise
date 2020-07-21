09a1d31d370083458b76d6e8f5ecd5662503b92f
AlexNexton <cianoc13@gmail.com>
2020-07-21 13:50:53 +0000

    mix and match working

--- assets/js/gamescript.js
+++ assets/js/gamescript.js
@@ -36,6 +36,8 @@ class AudioController{ // adding the audio for the game and the card sounds
 }
 //---------------------------end of Audion controller class------------------------------------------------------
 
+
+
 //---------------------------Start of mix or Match class------------------------------------------------------
 
 class MixorMatch{
@@ -58,13 +60,14 @@ class MixorMatch{
             this.audioController.startMusic();
             this.shuffleCards();
             this.countDown = this.startCountDown();
+            this.busy = false;
         }, 500);
         this.hideCards();
         this.timer.innerText = this.timeRemanining; // reseting timer and ticker
         this.ticker.innerText = this.totalClicks;
     }
     hideCards(){
-        this.cardsArray.forEach(card =>{
+        this.cardsArray.forEach(card => {
             card.classList.remove('vis');
             card.classList.remove('matched');
         });
@@ -78,22 +81,65 @@ class MixorMatch{
             this.ticker.innerText = this.totalClicks;
 
             card.classList.add('vis'); //flips the cards using class I created in html 'vis'
-            //if statement
+            
+
+            if(this.cardToCheck)//for matching cards
+                this.checkForCardMatch(card);
+            else
+                this.cardToCheck = card;
+            
         }
     }
+     checkForCardMatch(card){
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+           
+            this.cardMatch(card, this.cardToCheck);
+        else
+             this.cardMisMatch(card, this.cardToCheck);
+        
+        this.cardToCheck = null;
+
+        
+     }
+     cardMatch(card1, card2){
+        this.matchedCards.push(card1);
+         this.matchedCards.push(card2);
+         card1.classList.add('matched');
+         card2.classList.add('matched');
+         this.audioController.match();
+         if(this.matchedCards.length === this.cardsArray)
+         this.victory();
+     }
+     cardMisMatch(card1, card2){
+         //if there's a mis match the user gets a second before the cards flip back
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('vis');
+            card2.classList.remove('vis');
+            this.busy = false;
+        }, 1000);
+     }
+     getCardType(card){
+        return card.getElementsByClassName('card-value')[0].src;
+     }
     startCountDown(){
         return setInterval(() => {
             this.timeRemanining--; // decrements the clock
             this.timer.innerText = this.timeRemanining; //changes the html clock
-            if(this.timeRemanining === 0){
+            if(this.timeRemanining === 0)
                 this.gameOver();
-            }
+            
         }, 1000);
     }
-    gameOver(){
+    gameOver(){ //game over screen pops up
         clearInterval(this.countDown);
         this.audioController.gameOver();
         document.getElementById('game-over-text').classList.add('vis');
+    }
+    victory(){ //victory text pops up
+        clearInterval(this,this.countDown);
+        this.audioController.victory();
+         document.getElementById('victory-text').classList.add('vis');
     }
      shuffleCards(){ //fisher and yates shuffle - taken from the internet https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
             for(let i = this.cardsArray.length - 1; i > 0; i--)
@@ -103,13 +149,16 @@ class MixorMatch{
                 this.cardsArray[i].style.order = randIndex;
             }
         }
-    canFlipCard(card){ // if all three of these statements are false then this will return true
-        return true; // to see the card flipping for now
-        //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    canFlipCard(card){ // if all three of these statements are false then this will return true --to see the card flipping for now
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
     }
 }
 
 //---------------------------end of mix or match class------------------------------------------------------
+
+
+
+//---------------------------Start of Ready Function class------------------------------------------------------
 function ready(){
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
