9c455b9014101ea97897eb20a03210e01ba27145
AlexNexton <cianoc13@gmail.com>
2020-07-21 11:48:52 +0000

    added new clas to flip the cards and count the flips

--- assets/js/gamescript.js
+++ assets/js/gamescript.js
@@ -1,3 +1,5 @@
+//---------------------------Start of Audion controller class------------------------------------------------------
+
 class AudioController{ // adding the audio for the game and the card sounds
     constructor(){
             this.backgroundMusic = new Audio('assets/audio/Star Wars Cantina Band (Noize Tank Remix) - Gaming Background Music (HD).mp3');
@@ -8,28 +10,82 @@ class AudioController{ // adding the audio for the game and the card sounds
             this.backgroundMusic.volume = 0.5;
             this.backgroundMusic.loop = true;
 
-    }
+    } // functions to be called for the cards and background music
     startMusic(){
         this.backgroundMusic.play();
     }
+    stopMusic(){
+        this.backgroundMusic.pause();
+        this.backgroundMusic.currentTime = 0;
+    }
+    flip(){
+        this.flipSound.play();
+    }
+    match(){
+        this.matchSound.play();
+    }
+    victory(){
+        this.stopMusic(); //stops background music once the game is complete
+        this.victorySound.play();
+    }
+    gameOver(){
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
+
+}
+//---------------------------end of Audion controller class------------------------------------------------------
+
+//---------------------------Start of mix or Match class------------------------------------------------------
+
+class MixorMatch{
+    constructor(totalTime, cards){
+            this.cardsArray = cards;
+            this.totalTime = totalTime;
+            this.timeRemanining = totalTime;
+            this.timer = document.getElementById('time-remaining');
+            this.ticker = document.getElementById('flips');
+            this.audioController = new AudioController();
+    }
+    startGame(){
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemanining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+    }
+    flipCard(card){
+        if(this.canFlipCard(card)){
+            this.audioController.flip();
+            this.totalClicks++; // counts eachtime the cards are clicked
+            this.ticker.innerText = this.totalClicks;
+            card.classList.add('vis'); //flips the cards using class using class I created in html
+        }
+    }
+    canFlipCard(card){ // if all three of these statements are false then this will return true
+        return true; // to see the card flipping for now
+        //return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
 }
 
+//---------------------------end of mix or match class------------------------------------------------------
 function ready(){
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixorMatch(100, cards); // instance for card game #1
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click',() =>{
             overlay.classList.remove('vis');
-            // game.startGame();
-            let audioController = new AudioController();
-            audioController.startMusic();
+             game.startGame(); //starts the game #1
+            /*let audioController = new AudioController();
+            audioController.startMusic(); */
         });
     });
 
     cards.forEach(card => {
         card.addEventListener('click', () =>{
-            //game.flipCard(card);
+            game.flipCard(card); //flips a card #1
         });
     });
 } 
